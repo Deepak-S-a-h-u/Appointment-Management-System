7a9406b16a861ccd1e2d5076940597f6ca6e4831
Deepak_Sahu <programmer.zone101@gmail.com>
2023-01-11 12:34:30 +0530

    Backend Created

---
+++ Appointment_Management_System_Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AppointmentManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appointment_Management_System_Backend.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        /*public DbSet<Department> Departments { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+*/
+    }
+}
